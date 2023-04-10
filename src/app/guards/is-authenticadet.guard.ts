6eb2b2d7969290983c966f7c6dc9174ff7bff129
Samir1009ms <yusifovs1009@gmail.com>
2023-04-10 13:04:07 +0400

    task_management

--- src/app/guards/is-authenticadet.guard.ts
+++ src/app/guards/is-authenticadet.guard.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree } from "@angular/router";
+
 import { Observable} from "rxjs";
-import { AuthService } from "../services/auth.service";
 import {tap} from  'rxjs/operators'
+
+import { AuthService } from "../services/auth.service";
 
 @Injectable({
   providedIn: 'root'
